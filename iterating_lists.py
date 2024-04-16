474db37d4776baeaa174bf9e2031e21afa2c25d6
Younes <younesesebbar089@gmail.com>
2024-04-16 17:07:05 +0100

    finish the list section with a good exercice

--- iterating_lists.py
+++ iterating_lists.py
@@ -1,4 +1,4 @@
-emails = ["younes@gmail.com", "marwa@gmail.com", "hanane@gmail.com", "ayoub@gmail.com", "sara@gmail.com", "inas@gmail.com"]
+emails = ["younes@gmail.com", "marwa@gmail.com", "aymane@gmail.com", "ayoub@gmail.com", "sara@gmail.com", "inas@gmail.com"]
 
 
 # for char in "Hellow":
@@ -12,10 +12,23 @@ emails = ["younes@gmail.com", "marwa@gmail.com", "hanane@gmail.com", "ayoub@gmai
 #     print(emails[idx])
 #     idx += 1
 
-lando_2021_results = [4, 3, 4, 5, 3, 4, 8, 10, 4, 10, 7, 10, 2, 5, 5, 5, 9]
+lando_2021_results = [4, 3, 4, 5, 3, 4, 8, 10, 4, 10, 7, 10, 2, 5, 5, 5, 9,1]
 
-sum = 0
+# sum = 0
+# for num in lando_2021_results:
+#     sum += num
+# avg = round(sum / len(lando_2021_results))
+
+
+def average(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return round(total / len(nums))
+
+min = lando_2021_results[0]
 for num in lando_2021_results:
-    sum += num
+    if num < min:
+        min = num
 
-print(sum / len(lando_2021_results))
+print(min)
