9785e8b7d86b51a6f186d206f374b65707735636
Younes <younesesebbar089@gmail.com>
2024-09-28 11:10:35 +0100

    exercices using lambdas and built-in functions

--- lambdas and built-in functions/zip.py
+++ lambdas and built-in functions/zip.py
@@ -3,11 +3,25 @@ finals = [98, 89, 53]
 students = ["dan", "kate", "ibrahim"]
 
 # final_grades = {'dan': 98, 'kate': 91, 'ibrahim': 78}
-highest_grades = dict(zip(students, (max(pair) for pair in zip(midterms, finals))))
+# highest_grades = dict(zip(students, (max(pair) for pair in zip(midterms, finals))))
 # highest_grades = {pair[0]: max(pair[1], pair[2]) for pair in zip(students, midterms, finals)}
 
-average_grades = dict(zip(students, (sum(pair) // len(pair) for pair in zip(midterms, finals))))
+# average_grades = dict(zip(students, (sum(pair) // len(pair) for pair in zip(midterms, finals))))
 # average_grades = {pair[0]: (pair[1] + pair[2]) // 2 for pair in zip(students, midterms, finals)}
 
-print("Highest grades of this class: ", highest_grades)
-print("Average grades of this class: ", average_grades)
+# using built-in functions
+highest_grades = zip(students, map(lambda pair: max(pair), zip(midterms, finals)))
+average_grades = zip(students, map(lambda pair: sum(pair) // 2, zip(midterms, finals)))
+
+
+print("Highest grades of this class: ", dict(highest_grades))
+print("Average grades of this class: ", dict(average_grades))
+
+# interleave function
+def interleave(str1, str2):
+    """Takes 2 strings and concat each character from str1 with str2, taking in consideration their indices"""
+    return ''.join(''.join(s) for s in zip(str1, str2))
+
+interleave('hi','ha')    # 'hhia'
+interleave('aaa', 'zzz')  # 'azazaz'
+interleave('lzr','iad') #  'lizard'
